8eb2f8ed65051782325013cc11bc45f9c4536673
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-07 13:25:56 -0500

    Update amazon167.py

--- amazon167.py
+++ amazon167.py
@@ -12,7 +12,7 @@ Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
 """
 
-# Final Solution with time complexity of O(n):
+# Final Solution with time complexity of O(n) beats 83% runtime and 75% memory:
 class Solution(object):
     def twoSum(self, numbers, target):
         l = 0 
