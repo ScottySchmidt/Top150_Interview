a8ca652f4340dc832b2b52dca7ebe71dbcaca1ae
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-10 14:45:42 -0500

    Create facebook16.py

---
+++ facebook16.py
@@ -0,0 +1,38 @@
+"""
+18. 4Sum https://leetcode.com/problems/4sum/
+
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+"""
+
+# Passes 128 / 292 testcases passed:
+class Solution(object):
+    def fourSum(self, nums, target):
+        nums.sort()
+        quads = set()
+        n = len(nums)
+
+        for i in range(n-3):
+            for j in range(i+1, n-2):
+                start = j + 1
+                end = n-1
+                diff = target - nums[i] - nums[j]
+                while start < end:
+                    total = nums[start] + nums[end] + diff
+                    if total < target:
+                        start = start+1
+                    elif total > target:
+                        end = end -1
+                    else:
+                        quads.add((nums[i], nums[j], nums[start], nums[end]))     
+                        end=end-1
+                        start=start+1
+        return quads
