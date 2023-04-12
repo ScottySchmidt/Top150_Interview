8c69a3099a5ba166fe582d7fa0a20903f55900d7
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-12 12:26:25 -0500

    Update amazon17.py

--- amazon17.py
+++ amazon17.py
@@ -10,6 +10,23 @@ Input: digits = "23"
 Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 """
 
+# Final working solution, below average runtime could use improvement in future:
+class Solution(object):
+    def letterCombinations(self, digits):
+        if len(digits)==0:
+            return []
+        
+        phone_digits = {"2": "abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        res = [""]
+        for d in digits:
+            newList = []
+            letters = phone_digits[d]
+            for l in letters:
+                for r in res:
+                    newList.append(r+l)
+            res = newList
+        return res
+
 
 
 # Initial solution passes almost half the test cases but does not flatten the list depending on number of digits.
