4c756f39357bc578405aa61ae14e0212d822b666
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-20 16:21:26 -0500

    Update Apple42.py

--- Apple42.py
+++ Apple42.py
@@ -33,34 +33,3 @@ class Solution(object):
                 total=total+water
         return total
 
-
-# Original draft 318/322 solutions, but uses too much memory for complicated arrays:
-class Solution(object):
-    def trap(self, height):
-        r=len(height)
-
-        rightsMax=[]
-        
-        for i in range(0, r):
-            rights=height[i:r]
-            rMax=max(rights)
-            rightsMax.append(rMax)
-
-        #first left is always 0
-        leftsMax=[0]
-        for x in range(1, r):
-            lefts=height[0:x]
-            lMax=max(lefts)
-            leftsMax.append(lMax)
-
-        finalList=[]
-        for i in range(0, r):
-            low=min(rightsMax[i], leftsMax[i])
-            water=low-height[i]
-            finalList.append(water)
-        
-        #print(rightsMax, " rights")
-        #print(leftsMax, " lefts")
-        finalList = [0 if i < 0 else i for i in finalList]
-        #print(finalList, " final")
-        return sum(finalList)
