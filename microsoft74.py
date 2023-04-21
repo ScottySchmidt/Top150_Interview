e82ee9427a7826d7e858c22788695ba134bf9f30
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-21 07:25:15 -0500

    Create microsoft74.py

---
+++ microsoft74.py
@@ -0,0 +1,29 @@
+"""
+74. Search a 2D Matrix: https://leetcode.com/problems/search-a-2d-matrix/description/
+
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+"""
+
+# Accepted Solution Beats 50% runtime which uses a method checks each row
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m=len(matrix) # How many lists? 
+        for i in range(m):
+            if target in matrix[i]: # search each list
+                return True
+        return False
