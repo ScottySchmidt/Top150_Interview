d2b81425f8c2bb9fae781220ca98a9b62c5744a3
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-24 14:04:24 -0500

    Update google47.py

--- google47.py
+++ google47.py
@@ -11,6 +11,7 @@ Output:
  [2,1,1]]
 """
 
+# To beter understand see output at very end:
 class Solution(object):
     def permuteUnique(self, nums):
         res = [] # final answer
@@ -22,18 +23,51 @@ class Solution(object):
             count[n] += 1
         def search():
             if len(perm) == len(nums):  # if curlist equals lenth of nums, then a perm is found:
-                if perm[:] not in res:
-                    res.append(perm[:]) # must store a copy 
+                res.append(perm[:]) # must store a copy 
+                print(perm[:], " found")
                 return # not longer check
             
             for n in count:
                 if count[n] > 0:
                     perm.append(n)
+                    print(n, " added")
                     count[n] -= 1
                     
                     search() 
 
                     count[n] += 1
-                    perm.pop()       
+                    print(perm, " about to pop")
+                    perm.pop()
+                    print("pop ", perm)       
         search()
         return res
+       
+"""
+(1, ' added')
+(1, ' added')
+(2, ' added')
+([1, 1, 2], ' found')
+([1, 1, 2], ' about to pop')
+('pop ', [1, 1])
+([1, 1], ' about to pop')
+('pop ', [1])
+(2, ' added')
+(1, ' added')
+([1, 2, 1], ' found')
+([1, 2, 1], ' about to pop')
+('pop ', [1, 2])
+([1, 2], ' about to pop')
+('pop ', [1])
+([1], ' about to pop')
+('pop ', [])
+(2, ' added')
+(1, ' added')
+(1, ' added')
+([2, 1, 1], ' found')
+([2, 1, 1], ' about to pop')
+('pop ', [2, 1])
+([2, 1], ' about to pop')
+('pop ', [2])
+([2], ' about to pop')
+('pop ', [])
+"""
