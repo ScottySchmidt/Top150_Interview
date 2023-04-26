5e3252e2942d16faea5af3f3abcca316de58cfa4
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-26 14:18:39 -0500

    Create microsoft59.py

---
+++ microsoft59.py
@@ -0,0 +1,52 @@
+"""
+Micorosft 59. Spiral Matrix II: https://leetcode.com/problems/spiral-matrix-ii/description/
+
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+Example 1:
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+"""
+
+# Original draft, works but messes up number 5:
+class Solution(object):
+    def generateMatrix(self, n):
+        left = 0
+        right = n
+        top = 0
+        end = n
+        matrix = [ [0]*n for _ in range(3)]
+        count=1
+
+        while left <= right and top <= end:
+            # Create first row:
+            print("Going Right:")
+            for i in range(left, right):
+                matrix[top][i]=count
+                count=count+1
+                print(count)
+            top=top+1 # top row complete
+            
+            # Create last column:
+            print("Going Down:")
+            for i in range(top, end):
+                print(count)
+                matrix[i][right-1]=count
+                count=count+1
+            right=right-1 # right col complete
+
+            print("Going Left:")
+            for i in range(right, left, -1):
+                print(count)
+                matrix[end-1][i]=count
+                count=count+1
+            end=end-1 # bottom row complete
+            
+            print("Going Up:")
+            for i in range(end, top, -1):
+                print(count)
+                matrix[i][left]=count
+                count=count+1
+            left=left+1
+            
+        return matrix
