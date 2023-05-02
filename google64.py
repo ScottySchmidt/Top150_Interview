161f5bc0bc0348a184347ed4d878e191e89d4779
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-02 15:32:02 -0500

    Update google64.py

--- google64.py
+++ google64.py
@@ -14,6 +14,35 @@ Input: grid = [[1,2,3],[4,5,6]]
 Output: 12
 """
 
+# Final Accepted Solution but could use improved runtime:
+class Solution(object):
+    def minPathSum(self, grid):
+        import numpy as np
+        # print("original: ", grid)
+        m = len(grid)
+        n = len(grid[0])
+        copy = np.zeros((m, n)) #Create copy of original grid
+        copy[0][0] = grid[0][0] # set first num to original grid
+        
+        # first row accumulative sum:
+        for i in range(1, n):
+            #New Next    =   Prev  +  original num
+            copy[0][i]=copy[0][i-1] + grid[0][i]
+        
+        # first col accumlative sum:
+        for j in range(1, m):
+             #Next Col    = prev col  + original num 
+             copy[j][0] = copy[j-1][0] + grid[j][0]
+
+        # rest of grid, row, col:
+        for i in range(1, m):
+            for j in range(1, n):
+                #new num  = min(above or left) + original num
+                copy[i][j] = min(copy[i-1][j], copy[i][j-1]) + grid[i][j]  
+        #print(copy)
+        return int(copy[-1][-1])
+
+
 # Current draft is close with small bug:
 class Solution(object):
     def minPathSum(self, grid):
