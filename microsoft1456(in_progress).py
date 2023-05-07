4c5519c188a767e403dbbc5716a7b73fec9f6ab6
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-06 20:12:21 -0500

    Create microsoft1456(in_progress).py

---
+++ microsoft1456(in_progress).py
@@ -0,0 +1,42 @@
+"""
+Microsoft 1456. Maximum Number of Vowels in a Substring of Given Length
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+Example 1:
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+"""
+
+# Current Draft:
+class Solution(object):
+    def maxVowels(self, s, k):
+        n = len(s)/k
+        beg = 0
+        end = k
+        max_count = 0
+        count=0
+        vowels='aeiou'
+        while end < n:
+            sub = s[beg:end]
+            print(sub)
+            for l in sub:
+                if l in vowels:
+                    count += 1
+            if count > max_count:
+                max_count = count
+            beg += k
+            end += k
+            count = 0
+        return max_count
