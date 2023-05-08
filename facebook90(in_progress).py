d4da60db866753c0c0d8ffd364c82cd04f8d78ff
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-08 13:47:32 -0500

    Update facebook90(in_progress).py

--- facebook90(in_progress).py
+++ facebook90(in_progress).py
@@ -13,8 +13,13 @@ Input: nums = [0]
 Output: [[],[0]]
 """
 
-# Passes 3/20 Test Cases.
-# This solution appens back to the curList in a different index spot which throws off some test cases:
+
+
+"""
+Passes 3/20 Test Cases.
+This solution appens back to the curList in a different index spot which throws off some test cases.
+After some research, I overcomplicated this. Normally just append starting with small list to bigger list.
+"""
 class Solution(object):
     def subsetsWithDup(self, nums):
         ans=[[],nums]
