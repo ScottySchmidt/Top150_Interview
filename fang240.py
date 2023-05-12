b99cba1c6f81286043f4fca872310226d05ca9bb
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-12 08:39:13 -0500

    Create fang240.py

---
+++ fang240.py
@@ -0,0 +1,31 @@
+"""
+240. Search a 2D Matrix II  https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+  
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+  
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+"""
+
+# Beats 21% runtime, need faster solution?
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            if target in matrix[i]:
+                return True
+        else:
+            return False
