a405716f21325efc14c18e0c6be378bdb5e75786
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-13 10:04:20 -0500

    Update Facebook23.(in_process).py

--- Facebook23.(in_process).py
+++ Facebook23.(in_process).py
@@ -16,15 +16,15 @@ Merge all the linked-lists into one sorted linked-list and return it.
 class Solution(object):
     def mergeKLists(self, lists):
         v=[] 
-        for i in lists:
-            x=i # temp list
-            print(x)
-            while x:  # keep getting next item in list
-                v+=[x.val] # add val to list
-                x=x.next # next item
+        for lst in lists:
+            cur=lst # temp list
+            print(cur)
+            while cur:  # keep getting next item in list
+                v+=[cur.val] # add val to list
+                cur=cur.next # next item
         v=sorted(v,reverse=True) # backwards list for now
         print(v, " v")
         ans=None
         for i in v:
-            ans=ListNode(i,ans) # create a NodeList that will now be normal
+            ans=ListNode(i,ans) # i is value with next node ans
         return ans
