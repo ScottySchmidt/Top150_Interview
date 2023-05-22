d3c7a7b6011b93bd146f09f6cd280fe30a4c116b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-22 17:54:22 -0500

    Create slidingWindow208(in_progress).py

---
+++ slidingWindow208(in_progress).py
@@ -0,0 +1,39 @@
+"""
+209. Minimum Size Subarray Sum:  https://leetcode.com/problems/minimum-size-subarray-sum/
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+Example 1
+Input: target = 7, nums = [2,3,1,2,4,3]
+
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+"""
+
+# Passes 7/20 test cases:
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        n = len(nums)
+        total = 0
+        start = 0
+        minSubArr = 9999
+        
+        for end in range(n):
+            total += nums[end]
+            if total >= target:
+                length = end-start+1 # length of subarr
+                minSubArr = min(minSubArr, length) # store new min
+                start +=1
+        if minSubArr==9999:
+            return 0
+        else:
+            return minSubArr
