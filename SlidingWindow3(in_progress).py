62007a1be9a3455f6aa990754c3f1e2ba24ed4d5
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-05-23 22:07:13 -0500

    Update SlidingWindow3.py

--- SlidingWindow3(in_progress).py
+++ SlidingWindow3(in_progress).py
@@ -19,7 +19,26 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 """
 
-# 879 / 987 testcases passed
+# Final Accepted Solution with 77% runtime using sliding windows with 2 pointers:
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        beg = 0
+        end = 0
+        max_len = 0
+        for c in s:
+            word = s[beg:end]
+            if c in word:
+                cur_len = end-beg
+                beg = beg+word.index(c)+1
+                max_len = max(max_len, cur_len)
+            end += 1
+        if max_len < end-beg: # This happens when there is for instance only one char in s
+            return end-beg 
+        else:
+            return max_len
+
+
+# Attempt 879 / 987 testcases passed
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         beg = 0
