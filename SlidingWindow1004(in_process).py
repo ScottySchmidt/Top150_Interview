06d7b33803172492bec11a8f657abec3c84bb35b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-06-03 13:06:13 -0500

    Update SlidingWindow1004(in_process).py

--- SlidingWindow1004(in_process).py
+++ SlidingWindow1004(in_process).py
@@ -17,4 +17,23 @@ Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
 Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 """
 
-# Sliding Window Practice:
+# Sliding Window Accepted Solution, Need to reattempt from memory soon:
+class Solution(object):
+    def longestOnes(self, nums, k):
+        left = right = maxSize = 0 
+        n = len(nums)
+        # checks for edge cases: 
+        while left < n and right < n:
+            # while correct:
+            if k != 0 or nums[right] == 1:
+                if nums[right]==0:
+                    k = k -1 # used a remain
+                right = right+1
+            # incorrect, out of count:
+            elif k == 0:
+                if nums[left] == 0:
+                    k += 1
+                left = left+1 
+            curLen = right-left # get length
+            maxSize = max(maxSize, curLen) # store new size
+        return maxSize
