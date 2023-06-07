9d1944497e1a52460008913001603182a674b46b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-06-07 08:35:38 -0500

    Update and rename amazon152.py to dynamicProgramming152.py

--- dynamicProgramming152.py
+++ dynamicProgramming152.py
@@ -1,5 +1,5 @@
 """
-152. Maximum Product Subarray. https://leetcode.com/problems/maximum-product-subarray/
+152. Maximum Product Subarray. https://leetcode.com/problems/maximum-product-subarray/   Medium Amazon Interview Questions
 
 Given an integer array nums, find a subarray that has the largest product, and return the product.
 The test cases are generated so that the answer will fit in a 32-bit integer.
