69afca3dfaf44deb26b867ed2557460db27d9672
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-06-28 14:42:15 -0500

    Update PremiumvsFreemium.py

--- PremiumvsFreemium.py
+++ PremiumvsFreemium.py
@@ -35,33 +35,23 @@ rslt_df = groupDF[groupDF['diff'] > 0]
 rslt_df[['date', 'free',  'paying']]
 
 
+'''
+# SQL SOLUTION: 
+with temp as (SELECT d.date,
+sum(CASE 
+WHEN a.paying_customer='yes' THEN d.downloads ELSE 0
+END) as paying,
+sum(CASE 
+WHEN a.paying_customer='no' THEN d.downloads ELSE 0
+END) as free
+FROM ms_user_dimension u
+INNER JOIN ms_acc_dimension a
+ON u.acc_id = a.acc_id
+INNER JOIN ms_download_facts d 
+ON d.user_id = u.user_id
+GROUP BY d.date)
 
-# Initial Draft Solution Passes half of the rows correctly:
-import pandas as pd
-
-# Three Datasets:
-df = ms_user_dimension
-df2= ms_acc_dimension
-df3= ms_download_facts
-
-# Join Datasets:
-mergeDF = pd.merge(df, df2, on="acc_id")
-mergeDF = pd.merge(mergeDF, df3, on="user_id")
-#mergeDF.head()
-
-# Use Group By:
-groupDF=mergeDF.groupby(['date', 'paying_customer'])['downloads'].sum()
-
-# Three Datasets:
-df = ms_user_dimension
-df2= ms_acc_dimension
-df3= ms_download_facts
-
-# Join Datasets:
-mergeDF = pd.merge(df, df2, on="acc_id")
-mergeDF = pd.merge(mergeDF, df3, on="user_id")
-#mergeDF.head()
-
-# Use Group By:
-groupDF=mergeDF.groupby(['date', 'paying_customer']).agg({'downloads': 'sum'}).unstack() # unstack will act as a pivot
-groupDF=groupDF.reset_index().sort_values('date')
+SELECT date, paying, free
+FROM temp
+WHERE free > paying;
+'''
