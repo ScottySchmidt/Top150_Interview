de0a2fbf0c61b928d14be61b80c364f0cb06e0bb
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-06-28 09:52:35 -0500

    Update PremiumvsFreemium.py

--- PremiumvsFreemium.py
+++ PremiumvsFreemium.py
@@ -5,6 +5,36 @@ Find the total number of downloads for paying and non-paying users by date. Incl
 The output should be sorted by earliest date first and contain 3 columns date, non-paying downloads, paying downloads.
 DataFrames: ms_user_dimension, ms_acc_dimension, ms_download_factsExpected Output Type: pandas.DataFrame
 '''
+# Final Correct Solution:
+import pandas as pd
+
+# Three Datasets:
+df = ms_user_dimension
+df2= ms_acc_dimension
+df3= ms_download_facts
+
+# Join Datasets:
+mergeDF = pd.merge(df, df2, on="acc_id")
+mergeDF = pd.merge(mergeDF, df3, on="user_id")
+#mergeDF.head()
+
+# Use Group By:
+#groupDF=mergeDF.groupby(['date', 'paying_customer'])['downloads'].sum()
+
+# Use Group By:
+groupDF=mergeDF.groupby(['date', 'paying_customer']).agg({'downloads': 'sum'}).unstack()# unstack will act as a pivot
+groupDF=groupDF.reset_index().sort_values('date')
+#print(groupDF.columns) #MultiIndex([(date',''),   ('downloads',  'no'),  ('downloads', 'yes')]
+
+# Rename the columns directly
+groupDF.columns = ['date', 'free',  'paying']
+
+#Include only records where non-paying customers have more downloads than paying customers:
+groupDF['diff'] = groupDF['free']-groupDF['paying'].fillna(0)
+rslt_df = groupDF[groupDF['diff'] > 0]
+rslt_df[['date', 'free',  'paying']]
+
+
 
 # Initial Draft Solution Passes half of the rows correctly:
 import pandas as pd
