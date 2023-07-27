06ffd01194099c48220bce51e580633d5bb4283b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-07-27 14:49:43 -0500

    Update customerRevenue.py

--- customerRevenue.py
+++ customerRevenue.py
@@ -18,3 +18,21 @@ filtered_df = df[(df['order_date'].dt.year == year_to_filter) & (df['order_date'
 
 filtered_df = filtered_df.groupby('cust_id')['total_order_cost'].sum().reset_index(drop=False)
 filtered_df=filtered_df.sort_values(by='total_order_cost',  ascending=False)
+
+
+
+'''
+# SQL_Server Solution
+with cte as (
+select cust_id, sum(total_order_cost) as total_revenue
+FROM orders
+WHERE year(order_date) = '2019'
+AND month(order_date) = '3'
+GROUP BY cust_id
+)
+
+SELECT cust_id, total_revenue
+FROM cte
+ORDER BY total_revenue DESC
+;
+'''
