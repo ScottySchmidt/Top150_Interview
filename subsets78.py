27b5c8eb4ff8d14e1a56c8097ee44599f44a722d
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-12 14:26:38 -0600

    Update and rename facebook78.py to subsets78.py

--- subsets78.py
+++ subsets78.py
@@ -1,8 +1,16 @@
 """
-78. Subsets: Facebook https://leetcode.com/problems/subsets/description/
+78. Subsets https://leetcode.com/problems/subsets/description/
 
 Given an integer array nums of unique elements, return all possible subsets (the power set).
 The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
 """
 
 # Beats 89% runtime using backtrack:
@@ -10,7 +18,6 @@ class Solution(object):
     def subsets(self, nums):
         ans = [[]]
         end = len(nums)
-
         def search(curList, start):
             for i in range(start, end):
                 num = nums[i]
